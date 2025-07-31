4a4ef7177fc385678cf7c8204c018c4935d0d7d3
Mutarov <129605231+Mutarov@users.noreply.github.com>
2025-07-31 03:00:41 +0300

    Minor fixes

--- install-yay.sh
+++ install-yay.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-echo "Installing depencies..."
+echo "Installing dependecies..."
 sudo pacman -Syu
 sudo pacman -S --needed base-devel git
 echo "Cloning repository..."
-git clone https://aur.archlinux.org/yay.git ~/yay
-cd ~/yay
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
 echo "making package..."
 makepkg -si
 cd -
